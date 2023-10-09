657920add205ea1daa2f324f517f8b406b3dba77
Zbynek Nedoma <domaneni@gmail.com>
2023-10-09 15:05:38 +0200

    Added build folder

---
+++ build/info-points/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '41a63c73d37e31edb8ce');
---
+++ build/point-title/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a45332a9d77d2b277f38');
